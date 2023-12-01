723e30e60a4cb011ca74befadca728dda1548eba
fheck <florian.heck@promatis.de>
2023-12-01 14:03:49 +0100

    day one solution

---
+++ 2023/01/AoC1.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const readline = require('readline')
+
+let numberNameMap = new Map()
+const numberRegex = /(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)/g
+
+const func = async () => {
+    const stream = fs.createReadStream('Aufgabe1Data.txt');
+    const rl = readline.createInterface({
+        input: stream,
+        crlfDelay: Infinity
+    })
+
+    let sum = 0;
+    let lineCount = 0;
+    for await (let line of rl) {
+        const oldLine = line
+        lineCount++;
+        while (numberRegex.test(line)) {
+            line = line.replace(numberRegex, val => val[0] + numberNameMap.get(val) + val[val.length - 1])
+        }
+        const digits = line.split('').filter(char => !isNaN(char)).map(dig => parseInt(dig, 10));
+        const oldDigits = oldLine.split('').filter(char => !isNaN(char)).map(dig => parseInt(dig, 10));
+        const number = parseInt('' + digits[0] + '' + digits[digits.length - 1], 10);
+        const oldNumber = parseInt('' + oldDigits[0] + '' + oldDigits[oldDigits.length - 1], 10);
+        sum += number
+        if (oldLine !== line)
+            console.log(oldLine, oldNumber, line, Number(number))
+    }
+    console.log(sum, lineCount)
+}
+
+const setupMap = () => {
+    numberNameMap.set('one', 1);
+    numberNameMap.set('two', 2);
+    numberNameMap.set('three', 3);
+    numberNameMap.set('four', 4);
+    numberNameMap.set('five', 5);
+    numberNameMap.set('six', 6);
+    numberNameMap.set('seven', 7);
+    numberNameMap.set('eight', 8);
+    numberNameMap.set('nine', 9);
+}
+
+
+setupMap();
+func();
+
