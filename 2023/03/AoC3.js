0126f71485ac4b5d4406bfed5fd9414d8cc42c3d
HeggiG <florianheck0@gmail.com>
2023-12-03 23:21:55 +0100

    solution for part 1

--- 2023/03/AoC3.js
+++ 2023/03/AoC3.js
@@ -3,7 +3,7 @@
 const fs = require("fs");
 const readline = require("readline");
 
-const func = async () => {
+const func = async() => {
     const clamp = (num, low, hi) => {
         if (num < low) {
             return low;
@@ -15,8 +15,9 @@ const func = async () => {
     };
 
     const checkPerimeter = (arr, row, col, length) => {
-        for (let x = -1; x <= length; x++) {
-            for (let y = -1; y < 2; y++) {
+        for (let x = -1; x < 2; x++) {
+            for (let y = -1; y <= length; y++) {
+                console.log(clamp(row + x, 0, arr.length - 1), clamp(col + y, 0, arr[0].length - 1))
                 if (!/[0-9.]/.test(arr[clamp(row + x, 0, arr.length - 1)][clamp(col + y, 0, arr[0].length - 1)])) {
                     return true;
                 }
@@ -32,6 +33,11 @@ const func = async () => {
     });
 
     let arr = [];
+    let test = [
+        ['.', '.', '.', '.', '.'],
+        ['.', '1', '2', '2', '.'],
+        ['.', '.', '*', '.', '.']
+    ]
 
     for await (let line of rl) {
         arr.push(line.split(""));
@@ -41,16 +47,15 @@ const func = async () => {
     let length = 0;
     let number = "";
     let sum = 0;
-    for (let row = 0; row < arr.length; row++) {
-        for (let col = 0; col < arr[0].length; col++) {
-            if (/[0-9]/.test(arr[row][col])) {
-                number += "" + arr[row][col];
+    for (let row = 0; row < test.length; row++) {
+        for (let col = 0; col < test[0].length; col++) {
+            if (/[0-9]/.test(test[row][col])) {
+                number += "" + test[row][col];
                 found = true;
                 length++;
             }
-            if (found && !/[0-9]/.test(arr[row][col])) {
-                if (checkPerimeter(arr, row, col, length)) {
-                    console.log(Number(number));
+            if (found && !/[0-9]/.test(test[row][col])) {
+                if (checkPerimeter(test, row, col - (length), length)) {
                     sum += Number(number);
                 }
                 found = false;
