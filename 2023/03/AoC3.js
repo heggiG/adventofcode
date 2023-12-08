2b1d558b98a54a8e36eabfd82e9802663e01a7d1
fheck <florian.heck@promatis.de>
2023-12-08 15:45:11 +0100

    solution for part two day 3

--- 2023/03/AoC3.js
+++ 2023/03/AoC3.js
@@ -3,7 +3,7 @@
 const fs = require("fs");
 const readline = require("readline");
 
-const func = async() => {
+const func = async () => {
     const clamp = (num, low, hi) => {
         if (num < low) {
             return low;
@@ -15,15 +15,15 @@ const func = async() => {
     };
 
     const checkPerimeter = (arr, row, col, length) => {
+        let results = []
         for (let x = -1; x < 2; x++) {
             for (let y = -1; y <= length; y++) {
-                console.log(clamp(row + x, 0, arr.length - 1), clamp(col + y, 0, arr[0].length - 1))
-                if (!/[0-9.]/.test(arr[clamp(row + x, 0, arr.length - 1)][clamp(col + y, 0, arr[0].length - 1)])) {
-                    return true;
+                if (arr[clamp(row + x, 0, arr.length - 1)][clamp(col + y, 0, arr[0].length - 1)] === '*') {
+                    results.push({ x: clamp(row + x, 0, arr.length - 1), y: clamp(col + y, 0, arr[0].length - 1) });
                 }
             }
         }
-        return false;
+        return results;
     };
 
     const stream = fs.createReadStream("GameInput.txt");
@@ -33,11 +33,6 @@ const func = async() => {
     });
 
     let arr = [];
-    let test = [
-        ['.', '.', '.', '.', '.'],
-        ['.', '1', '2', '2', '.'],
-        ['.', '.', '*', '.', '.']
-    ]
 
     for await (let line of rl) {
         arr.push(line.split(""));
@@ -47,16 +42,27 @@ const func = async() => {
     let length = 0;
     let number = "";
     let sum = 0;
-    for (let row = 0; row < test.length; row++) {
-        for (let col = 0; col < test[0].length; col++) {
-            if (/[0-9]/.test(test[row][col])) {
-                number += "" + test[row][col];
+
+    let map = new Map();
+
+    for (let row = 0; row < arr.length; row++) {
+        for (let col = 0; col < arr[0].length; col++) {
+            if (/[0-9]/.test(arr[row][col])) {
+                number += "" + arr[row][col];
                 found = true;
                 length++;
             }
-            if (found && !/[0-9]/.test(test[row][col])) {
-                if (checkPerimeter(test, row, col - (length), length)) {
-                    sum += Number(number);
+            if (found && (!/[0-9]/.test(arr[row][col]) || col > arr[0].length)) {
+                if (+number === 795) console.log('ABCV')
+                let stars
+                if ((stars = checkPerimeter(arr, row, col - (length), length)).length > 0) {
+                    if (+number === 795) console.log('ABCV')
+                    for (let star of stars) {
+                        if (!map.has(toKey(star.x, star.y))) {
+                            map.set(toKey(star.x, star.y), [])
+                        }
+                        map.get(toKey(star.x, star.y)).push(+number)
+                    }
                 }
                 found = false;
                 number = "";
@@ -65,7 +71,18 @@ const func = async() => {
         }
     }
 
-    console.log(sum);
+
+    for (let val of map.values()) {
+        if (val.length == 2) {
+            sum += val[0] * val[1];
+        }
+    }
+
+    console.log(sum)
 };
+
+const toKey = (x, y) => {
+    return '' + x + ',' + y;
+}
 
 func();
